64e57d2c2c7d01e4f85696ba213181cc1f98b61c
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-02-06 09:55:21 -0700

    Made my node hpp file

---
+++ DataStructures2420/Model/Nodes/Node.hpp
@@ -0,0 +1,44 @@
+//
+//  Node.hpp
+//  DataStructures2420
+//
+//  Created by Bevan, Tyler on 2/6/18.
+//  Copyright © 2018 Bevan, Tyler. All rights reserved.
+//
+
+#ifndef Node_hpp
+#define Node_hpp
+
+class Node
+{
+private:
+    
+protected:
+    Type data;
+public:
+    Node();
+    Node(Type data);
+    void setData(Type data);
+    Type getData();
+};
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this -> data = data;
+}
+
+template <class Type>
+void Node<Type> ::setData(Type data)
+{
+    this -> data = data;
+}
+
+template <class Type>
+Type Node<Type> :: getData()
+{
+    return data;
+}
+
+
+#endif /* Node_hpp */
