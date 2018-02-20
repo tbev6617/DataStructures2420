a6eed8e54ccebe138da24c1f3efdfa2fb76a7cf7
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-02-20 08:24:04 -0700

    fixed my List Class

--- DataStructures2420/Model/Structures/List.hpp
+++ DataStructures2420/Model/Structures/List.hpp
@@ -12,15 +12,17 @@
 #include <assert.h>
 #include "../../Nodes/LinearNode.hpp"
 
-template class List<Type>
+
+class List
 {
 protected:
     int size;
 public:
     //Structure
     virtual void add(Type item) = 0;
-    virtual void assAtIndex(int index, Type item) = 0;
+    virtual void addAtIndex(int index, Type item) = 0;
     virtual Type remove(int index) = 0;
+    virtual Type getFromIndex(int index) = 0;
     
     //Helper
     virtual int getSize() const = 0;
