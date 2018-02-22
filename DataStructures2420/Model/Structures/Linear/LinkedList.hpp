b9f91bb04ba842db1bfd1cd26c3ab0da2be0b5b5
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-02-22 09:17:59 -0700

    made the stack file

--- DataStructures2420/Model/Structures/Linear/LinkedList.hpp
+++ DataStructures2420/Model/Structures/Linear/LinkedList.hpp
@@ -39,6 +39,10 @@ public:
     Type remove(int index);
     //Type setAtIndex(int index, Type item);
     //bool contains(Type item);
+    virtual void add(Type item);
+    virtual void addAtIndex(int index, Type item);
+    virtual Type getFromIndex(int index);
+    virtual Type remove(int index;
 }
 LinkedList<Type> :: LinkedList()
 {
