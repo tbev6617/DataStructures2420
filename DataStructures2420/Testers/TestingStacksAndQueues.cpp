09015d4d71c975ccbb88a265ba39be28fec31a0f
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-06-01 08:13:26 -0600

    TestingStacksAndQueues was added

--- DataStructures2420/Testers/TestingStacksAndQueues.cpp
+++ DataStructures2420/Testers/TestingStacksAndQueues.cpp
@@ -10,12 +10,76 @@
 
 void TestingStacksAndQueues :: queueVersusList()
 {
+    Queue<string> wordQueue;
+    LinkedList<string> wordList;
     
+    wordQueue.add("first");
+    wordQueue.add("second");
+    wordQueue.add("third");
+    wordQueue.add("fourth");
+    wordQueue.add("fifth");
+    
+    wordList.add("first");
+    wordList.add("second");
+    wordList.add("third");
+    wordList.add("fourth");
+    wordList.add("fifth");
+    
+    cout << "Queue: " << wordQueue.getFromIndex(0) << " versus List: " << wordList.getFromIndex(0) << endl;
+    cout << "Here is in order removal in a queue: " << endl;
+    wordQueue.clear();
+    cout << "Here is a backwards order removal in a list:" << endl;
+    for(int i = wordList.getSize() - 1; i >= 0; i--)
+    {
+        cout << wordList.remove(i) << endl;
+    }
 }
 
 void TestingStacksAndQueues :: stackVersusQueue()
 {
-    //TODO pg 4
+    vector<CrimeData> data = FileController :: readCrimeDataToVector("/Users/tbev6617/Documents/Data Structures/DataStructures2420/DataStructures2420/Data/crime.csv");
+    
+    Stack<CrimeData> crimeStack;
+    Queue<CrimeData> crimeQueue;
+    
+    for(int index = 0; index < 100; index++)
+    {
+        CrimeData temp = data.at(index);
+        crimeStack.push(temp);
+        crimeQueue.enqueue(temp);
+    }
+    Queue<int> queue;
+    Stack<int> stack;
+    Queue<int> queueFromStack;
+    Stack<int> stackFromQueue;
+    
+    for(int i = 0; i < 10; i++)
+    {
+        queue.enqueue(i);
+        stack.push(i);
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+        cout << "Queue result: " << queue.dequeue() << endl;
+        cout << "Stack result: " << stack.pop() << endl;
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        queue.enqueue(i);
+        stack.push(i);
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        stackFromQueue.push(queue.dequeue());
+        queueFromStack.enqueue(stack.pop());
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        cout << i << endl;
+        cout << "Stack result: " << stackFromQueue.pop() << endl;
+        cout << "Queue result: " << queueFromStack.dequeue() << endl;
+    }
 }
 
 void TestingStacksAndQueues :: stackVersusList()
